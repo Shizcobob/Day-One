ae25c2c3dfbd7f0137e52ebdd74730b997f5f159
Shizcobob <kyle.korinek@outlook.com>
2023-06-06 12:06:50 -0400

    submission

---
+++ reverseString.js
@@ -0,0 +1,49 @@
+ /* 
+    String: Reverse
+    Given a string,
+    return a new string that is the given string reversed
+*/
+
+const str1 = "creature";
+const expected1 = "erutaerc";
+
+const str2 = "dog";
+const expected2 = "god";
+
+const str3 = "hello";
+const expected3 = "olleh";
+
+const str4 = "";
+const expected4 = "";
+
+/**
+ * Reverses the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str String to be reversed.
+ * @returns {string} The given str reversed.
+ */
+
+
+
+
+function reverseString(str) {
+    var temp = "";
+    // let xyz = str.split('');
+
+    for(var i=0; i< str.length; i++){
+       temp = str[i]+temp;
+    }
+    return temp 
+}
+
+console.log(reverseString("hello"));
+
+//TEST CODE FOR REVERSE
+console.log(reverseString(str1)) // Expected: erutaerc
+console.log(reverseString(str2)) // Expected: god
+console.log(reverseString(str3)) // Expected: olleh
+console.log(reverseString(str4)) // Expected: ""
+
+// try to do it without any built in methods!
+// try to do it looping forwards only!
